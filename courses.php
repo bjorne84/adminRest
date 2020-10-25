540bcc247994a2f3b0618fd8acf0bfcd89037a3a
bjorne84 <edinbjorn@gmail.com>
2020-10-25 20:13:12 +0100

    function för att hämta skola och få in datan i dropdown select i forumulär på skapoa ny kurs

--- courses.php
+++ courses.php
@@ -4,28 +4,33 @@ include('includes/header.php');
 <main>
     <div class="mainWrapper">
     <p>Add a new Course to database</p>
-    <form class="forms" id="formCreate" method="POST" action="index.html" enctype="multipart/form-data" forum-action>
-        <!--fält för formulär, hela den grå delen-->
-        <fieldset id="field">
-            <p class="pfield pError" id="message_form"></p>
-            <label for="name">N:</label><br>
-            <input type="text" name="name" id="name" class="input" placeholder="The name of the Course">
-            <br>
-            <label for="code">Code:</label><br>
-            <input type="text" name="code" id="code" class="input" placeholder="The code of the course, 6 signs">
-            <br>
-            <label for="progression">Course:</label><br>
-            <input type="text" name="progression" id="progression" class="input" placeholder="Pagenation, one letter">
-            <br>
-            <label for="kursplan">Course syllabus:</label><br>
-            <input type="text" name="kursplan" id="kursplan" class="input" placeholder="Web-link to the course syllabus"><br>
-            <div class="btn-wrapper">
-                <button type="submit" name="submitPost" id="btncreate" class="btn btn2">Publish</button>
-                <button type="reset" name="deletePost" id="btn-reset" class="btn btn2 btn-reset">Delete
-                    field</button>
-            </div>
-        </fieldset>
-    </form>
+        <form class="forms" id="formCreate2" method="POST" action="index.html" enctype="multipart/form-data" forum-action>
+            <!--fält för formulär, hela den grå delen-->
+            <fieldset id ="field2" class="field2">
+                <p class="pfield pError" id="message_form"></p>
+                <label for="programme">Choose programme:</label>
+                <br>
+                <select id="programme" name="programme">
+                </select>
+                <br>
+                <label for="name">Course:</label><br>
+                <input type="text" name="course:" id="course" class="input" placeholder="The name of the Course">
+                <br>
+                <label for="code">Code:</label><br>
+                <input type="text" name="code" id="code" class="input" placeholder="The code of the course, 6 signs">
+                <br>
+                <label for="progression">Points:</label><br>
+                <input type="text" name="points" id="points" class="input" placeholder="add points">
+                <br>
+                <label for="kursplan">Grade:</label><br>
+                <input type="text" name="grade" id="grade" class="input" placeholder="add grade"><br>
+                <div class="btn-wrapper">
+                    <button type="submit" name="submitPost" id="btncreate" class="btn btn2">Publish</button>
+                    <button type="reset" name="deletePost" id="btn-reset" class="btn btn2 btn-reset">Delete
+                        field</button>
+                </div>
+            </fieldset>
+        </form>
     </div>
     <section class="sectionPadding">
         <h1>Utbildningar:</h1>
--- js/getMethod.js
+++ js/getMethod.js
@@ -1,8 +1,12 @@
 // Element för att läsa ut alla kurser, uppdatera och radera 
 let courseEl = document.getElementById('courseWrapper');
+// element för att välja program/utbildning
+let programmeEl = document.getElementById('programme');
 
 // Händelselyssnare, när sidan har laddat klart
 window.addEventListener('load', getCourse);
+window.addEventListener('load', getEducationToSelect);
+
 //Händelselyssnare för formuläret, skapa ny kurs
 
 // ***** Funktioner ************
@@ -15,7 +19,6 @@ function getCourse() {
         .then(response => response.json()
             .then(data => {
                 data.forEach(course => {
-                    console.log(course);
                     courseEl.innerHTML +=
                         `<div class="getcourse">
                             <ul class="ul">
@@ -34,3 +37,21 @@ function getCourse() {
             })
         )
 }
+
+
+function getEducationToSelect() {
+        // Get data over courses and education from this url
+        fetch('https://webb01.se/restapi/?table=education')
+        .then(response => response.json()
+            .then(data => {
+                data.forEach(education => {
+                    //console.log(education);
+                    // info insereted in form select, as dropdown
+                    programmeEl.innerHTML +=
+                        `<option value="${education.Education_ID}<">${education.Programme}</option>
+                        `
+                })
+            })
+        )
+}
+
